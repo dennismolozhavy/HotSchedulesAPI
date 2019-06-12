e82f2d397943d21446450cfe1ad08bf0f13d4f86
Dennis Molozhavy <51740893+dennismolozhavy@users.noreply.github.com>
2019-06-12 13:39:10 +0300

    Create Quote.cs

---
+++ HotSchedulesAPI.Sources/Quote.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace HotSchedulesAPI.Sources
+{
+    public class Quote
+    {
+        public DateTime QuoteDate { get; set; }
+        public string Symbol { get; set; }
+        public decimal Open { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal Close { get; set; }
+        public decimal AdjustedClose { get; set; }
+        public int Volume { get; set; }
+        public decimal Dividend { get; set; }
+        public decimal SplitCoefficient { get; set; }
+
+        public byte[] Serialize()
+        {
+            string json = JsonConvert.SerializeObject(this);
+            return Encoding.ASCII.GetBytes(json);
+        }
+    }
+}
